458c80db3fb5bb73a235067780f1e758d604b453
diasre001 <renan.dias@prometeon.com>
2025-04-24 21:26:40 -0300

    init

---
+++ next.config.js
@@ -0,0 +1,14 @@
+```javascript
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/prometeon-site/',
+  basePath: '/prometeon-site',
+};
+
+module.exports = nextConfig;
+```
